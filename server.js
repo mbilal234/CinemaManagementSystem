a18c0ebc7852c666788010152c5ac8b8bb66ec51
unknown <abdul.arham9123@gmail.com>
2022-11-28 16:11:49 +0500

    Made Dynamic Select Options and deleted unnecessary JS files

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const fetch = require("node-fetch");
 const app = express();
 const path = require("path");
 const bodyParser = require('body-parser');
-const insertCustomer = require("./JS/dml.js")
-const insertFilm = require("./JS/ifd.js");
-const retrieveFilm = require("./JS/filmRetrieval.js")
 
 app.set("view-engine", "ejs");
 
@@ -14,12 +11,71 @@ app.use('/JS', express.static('JS'));
 app.use('/images', express.static('images'));
 app.use(bodyParser.urlencoded({ extended: false }))
 
-async function openSeeMore(film_id, res){
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "abdularham123",
+    database: "cinema",
+    insecureAuth: true
+});
+
+async function openSeeMore(film_id, res) {
     const result = await retrieveFilm(film_id);
     // fetch(result[0][6]);
-    res.render("viewFilm.ejs", {name: result[0].title, cover_img: result[0].cover_img, desc_: result[0].desc_,
-    run_time: result[0].run_time, rating: result[0].rating, genre: result[0].genre, start_date: result[0].start_date, 
-    end_date: result[0].end_date});
+    res.render("viewFilm.ejs", {
+        name: result[0].title, cover_img: result[0].cover_img, desc_: result[0].desc_,
+        run_time: result[0].run_time, rating: result[0].rating, genre: result[0].genre, start_date: result[0].start_date,
+        end_date: result[0].end_date
+    });
+}
+
+function insertFilm(film_id, title, genre, desc_, run_time, rating, cover_img, start_date, end_date) {
+    film_id = parseInt(film_id);
+    run_time = parseInt(film_id);
+    var values = [[film_id, title, genre, desc_, run_time, rating, cover_img, start_date, end_date]];
+    fetch("http://localhost:3000/images/"+cover_img).then((res)=>{
+        return res.blob;
+    }).then((blob) => {
+        values[0][6] = blob;
+    });
+    con.query("insert into dummy_films (film_id, title, genre, desc_, run_time, rating, cover_img, start_date, end_date) values (?)", values, (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log(result);
+    })
+
+}
+
+async function retrieveFilm(film_id) {
+    const result = await new Promise((resolve) => {
+        con.query("SELECT * FROM dummy_films where film_id = (?)", [film_id], (err, res) => {
+            if (err) throw err;
+            resolve(res);
+        })
+    })
+    const imageUrl = await new Promise((resolve)=>{
+        con.query("SELECT convert(cover_img using utf8) FROM dummy_films where film_id = (?)", [film_id], (err, res) => {
+            if (err) throw err;
+            resolve(res)
+        })
+    })
+    // console.log(imageUrl);
+    result[0].cover_img = "http://localhost:3000/images/"+imageUrl[0]["convert(cover_img using utf8)"];
+    return result;
+}
+
+
+function getOptions(res) {
+    con.query("SELECT * FROM dummy_films", function (err, result, fields) {
+        if (err) throw err;
+        res.render("booking.ejs", { result: result, iterator: result.length });
+        return;
+    });
+    // console.log(result);
+    
 }
 
 app.get("/", (req, res) => {
@@ -27,7 +83,7 @@ app.get("/", (req, res) => {
 })
 
 app.get("/booking", (req, res) => {
-    res.render("Booking.ejs");
+    getOptions(res);
 })
 
 app.get("/registerMemberPage", (req, res) => {
@@ -39,7 +95,6 @@ app.post("/seeMore", (req, res) => {
 })
 
 app.post("/signup", (req, res) => {
-    insertCustomer(req.body.firstName, req.body.lastName);
     console.log("Hello World")
     res.redirect("/booking");
 })
