5e1453d39c618752fa41b66607a7676fd8621ae6
mbilal234 <mbilal.bese21seecs@seecs.edu.pk>
2022-12-16 09:36:46 +0500

    Added deselect button

--- JS/scripts.js
+++ JS/scripts.js
@@ -17,3 +17,33 @@ var count=0;
         }
       })
     }
+    item.addEventListener("dblclick",(event)=>{
+      if (event.target.classList.contains('selected') ){
+        count--;
+        var total=count*price;
+        event.target.classList.remove("selected");
+        document.getElementById("count").innerText=count;
+        document.getElementById("total").innerText=total;
+      }
+    });
+// Add a button to deselect all seats
+var deselectButton = document.createElement("button");
+deselectButton.innerText = "Deselect All";
+deselectButton.addEventListener("click", () => {
+  // Remove the "selected" class from all seats
+  for (var i = 0; i < seats.length; i++) {
+    var item = seats[i];
+    if (item.classList.contains('selected')) {
+      item.classList.remove("selected");
+    }
+  }
+  // Reset the count and total variables
+  count = 0;
+  total = 0;
+  document.getElementById("count").innerText = count;
+  document.getElementById("total").innerText = total;
+});
+
+// Add the button to the DOM
+document.body.appendChild(deselectButton);
+    
